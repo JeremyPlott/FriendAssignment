4b0fd8cda8cc9892c2cb24077fad5f0f5e5c6a48
Jeremy <JeremyPlott@gmail.com>
2019-08-29 14:45:50 -0400

    initial. added ternary operator.

---
+++ Friends/Friend.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Friends {
+    class Friend {
+
+        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~PROPERTIES~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+        private static int nextFriendNumber = 0;
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; } = null;
+        public string Email { get; set; } = null;
+        public bool BFF { get; set; } = false;
+        public double HowLong { get; set; }
+
+        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~METHODS~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+        public override string ToString() {
+            return $"{Name,-15} {Phone,-12} {Email,-25} {(BFF ? "BFF" : ""),3} {HowLong}";        
+        }
+
+        //defining what happens when someone calls ToString on the class + formatting.
+        //the numbers are the character width for each value. - means left-justified.
+        //BFF shows how to replace boolean value with T:F strings. Called the Ternary Operator. Can be used with other things:
+        //var a = (boolean condition)?c:d; set a = c if boolean is true. set a = d if boolean is false.
+        //using var a like above means that the 'var' interpreted variable will take on whatever type c and d are. one string, one dec, it will change a to that type.
+
+        //var a = ( b >= 0 ) ? c : d;
+
+        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~CONSTRUCTORS~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+        private void Initialize() {
+            Id = ++nextFriendNumber;
+        }
+
+        public Friend(string Name) {
+            this.Name = Name;
+            Initialize();
+        }
+
+        public Friend(string Name, string Phone, string Email, bool BFF, double HowLong) {
+            this.Name = Name;
+            this.Phone = Phone;
+            this.Email = Email;
+            this.BFF = BFF;
+            this.HowLong = HowLong;
+            Initialize();
+        }
+    }
+}
--- Friends/Program.cs
+++ Friends/Program.cs
@@ -3,7 +3,29 @@
 namespace Friends {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+
+            //create 5 friends
+
+            var friend1 = new Friend("Corey M", "513-555-1234", "ccccc@aol.com", true, 10.5);
+            var friend2 = new Friend("Chris M", "513-555-4321", "cMMMc@gmail.com", true, 11);
+            var friend3 = new Friend("Raymond K", "513-473-2354", "RrRrRr@yahoo.com", false, 0.8);
+            var friend4 = new Friend("Jacob G", "678-323-3476", "JGNo1@phishing.net", false, 1.2);
+            var friend5 = new Friend("Andrew 'AJ' R", "513-235-9999", "notenoughlette@gmail.com", true, 9.5);
+
+            //Console.WriteLine($"{friend1.Name} {friend2.Name} {friend3.Name} {friend4.Name} {friend5.Name}");
+
+            var friends = new Friend[] { friend1, friend2, friend3, friend4, friend5 };
+            foreach(var friend in friends) {
+                Console.WriteLine(friend);
+            }
+
+            var totalYrs = 0.0;
+            foreach(var friend in friends) {
+                totalYrs += friend.HowLong;
+            }
+            Console.WriteLine($"The total length of your friendships is {totalYrs} years. Not bad for someone with a computer monitor tan at that level.");
+
+            //console.WriteLine is already calling a function ToString when creating strings.
         }
     }
 }
